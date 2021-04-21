2586ce85948fb97d1db0f2ad71ce17c5d9d18c6a
elliott <elliott@email.get.com.tw>
2021-04-21 09:50:21 +0000

    fix active script

--- active.sh
+++ active.sh
@@ -1,4 +1,4 @@
 cp .emacs ~/.emacs
 cp -r .emacs.d ~/.emacs.d
-sudo chown -Rf $USER:$USER .emacs
-sudo chown -Rf $USER:$USER .emacs.d/
+sudo chown -Rf $USER:$USER ~/.emacs
+sudo chown -Rf $USER:$USER ~/.emacs.d/
